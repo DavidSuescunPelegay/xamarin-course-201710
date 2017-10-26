dcd74b94ca212302956a1ddd78f8dffd11f0c62e
David Suescun Pelegay <dsuescun96@gmail.com>
2017-10-26 21:15:04 +0200

    Updated seed/seed_data_app.cs

--- seed/seed_data_app.cs
+++ seed/seed_data_app.cs
@@ -1,5 +1,48 @@
-﻿new Client{City = "Zaragoza",Name = "Pepe Perez",Country = "España",Image = "http://person.eu/00general/img/coordinator_and_members/Sharon-Smit.png",ClientId = 1,Email = "pepe.perez@gmail.com"},
-new Client{City = "Huesca",Name = "Maria Luisa Fernandez",Country = "España",Image = "http://person.eu/00general/img/coordinator_and_members/Sharon-Smit.png",ClientId = 2,Email = "m.luisa.fernandez@hotmail.com"},
-new Client{City = "Teruel",Name = "Antonio Sanchez",Country = "España",Image = "http://person.eu/00general/img/coordinator_and_members/Sharon-Smit.png",ClientId = 3,Email = "sanchez.antonio@yahoo.es"},
-new Client{City = "Alcañiz",Name = "Pedro Ruiz",Country = "España",Image = "http://person.eu/00general/img/coordinator_and_members/Sharon-Smit.png",ClientId = 4,Email = "pedroruiz@gmail.com"},
-new Client{City = "Barbastro",Name = "Lucia Perez",Country = "España",Image = "http://person.eu/00general/img/coordinator_and_members/Sharon-Smit.png",ClientId = 5,Email = "lucia_perez@gmail.com"}
+﻿_clients = new List<Client>
+            {
+                new Client
+                {
+                    City = "Zaragoza",
+                    Name = "Pepe Perez",
+                    Country = "España",
+                    Image = "http://person.eu/00general/img/coordinator_and_members/Sharon-Smit.png",
+                    ClientId = 1,
+                    Email = "pepe.perez@gmail.com"
+                },
+                new Client
+                {
+                    City = "Huesca",
+                    Name = "Maria Luisa Fernandez",
+                    Country = "España",
+                    Image = "http://person.eu/00general/img/coordinator_and_members/Sharon-Smit.png",
+                    ClientId = 2,
+                    Email = "m.luisa.fernandez@hotmail.com"
+                },
+                new Client
+                {
+                    City = "Teruel",
+                    Name = "Antonio Sanchez",
+                    Country = "España",
+                    Image = "http://person.eu/00general/img/coordinator_and_members/Sharon-Smit.png",
+                    ClientId = 3,
+                    Email = "sanchez.antonio@yahoo.es"
+                },
+                new Client
+                {
+                    City = "Alcañiz",
+                    Name = "Pedro Ruiz",
+                    Country = "España",
+                    Image = "http://person.eu/00general/img/coordinator_and_members/Sharon-Smit.png",
+                    ClientId = 4,
+                    Email = "pedroruiz@gmail.com"
+                },
+                new Client
+                {
+                    City = "Barbastro",
+                    Name = "Lucia Perez",
+                    Country = "España",
+                    Image = "http://person.eu/00general/img/coordinator_and_members/Sharon-Smit.png",
+                    ClientId = 5,
+                    Email = "lucia_perez@gmail.com"
+                }
+            };
